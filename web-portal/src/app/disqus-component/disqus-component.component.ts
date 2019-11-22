070684694460d3ddc3e0b7ee3ccd2c985355ed43
Jyoti Duwal <djyoti@insightworkshop.io>
2019-11-22 13:58:09 +0545

    Implemented social login with google and shareability in twitter and facebook.

---
+++ web-portal/src/app/disqus-component/disqus-component.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-disqus-component',
+  templateUrl: './disqus-component.component.html',
+  styleUrls: ['./disqus-component.component.css']
+})
+export class DisqusComponentComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
