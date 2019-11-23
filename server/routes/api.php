94318a2c49c5adf9b0a3e2e4813ed134cdc77f74
ssuresh <ssureshiw@gmail.com>
2019-11-23 11:19:59 +0545

    Updated Category API

--- server/routes/api.php
+++ server/routes/api.php
@@ -23,7 +23,7 @@ Route::group(['middleware'=>'authenticate_token'], function(){
 
 
 Route::get('book-list','API\BookController@bookListApi')->name('get_book_name');
-Route::get('category','API\CategoryController@CategoryApi')->name('get_category');
+Route::get('categories','API\CategoryController@CategoryApi')->name('get_category');
 Route::get('user/{user_id}/other-books/{book_id}', 'API\BookController@otherBookList')
     ->name('other_book_list');
 
